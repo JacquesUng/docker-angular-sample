517e30505eba9cc89d3ceac9ca7dcf94bce76c4f
JacquesUng <jacques.ung@gmail.com>
2025-07-21 19:14:53 +0200

    Initial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,7 +25,7 @@ describe("AppComponent", () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector("h1")?.textContent).toContain(
-      "Docker Angular Sample Application"
+      "Hello from Docker Compose Watch"
     );
   });
 });
